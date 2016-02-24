1800fde2514083027fceed20cc9ff9891cda49ba
Rokyed <rokyed@gmail.com>
2016-02-23 20:10:48 -0500

    [removed modules that i don't require]

--- index.js
+++ index.js
@@ -8,49 +8,49 @@ exports.DataView = require('./lib/DataView');
 exports.Queue = require('./lib/Queue');
 
 // Graph3d
-exports.Graph3d = require('./lib/graph3d/Graph3d');
-exports.graph3d = {
-  Camera: require('./lib/graph3d/Camera'),
-  Filter: require('./lib/graph3d/Filter'),
-  Point2d: require('./lib/graph3d/Point2d'),
-  Point3d: require('./lib/graph3d/Point3d'),
-  Slider: require('./lib/graph3d/Slider'),
-  StepNumber: require('./lib/graph3d/StepNumber')
-};
+// exports.Graph3d = require('./lib/graph3d/Graph3d');
+// exports.graph3d = {
+//   Camera: require('./lib/graph3d/Camera'),
+//   Filter: require('./lib/graph3d/Filter'),
+//   Point2d: require('./lib/graph3d/Point2d'),
+//   Point3d: require('./lib/graph3d/Point3d'),
+//   Slider: require('./lib/graph3d/Slider'),
+//   StepNumber: require('./lib/graph3d/StepNumber')
+// };
 
 // Timeline
-exports.Timeline = require('./lib/timeline/Timeline');
-exports.Graph2d = require('./lib/timeline/Graph2d');
-exports.timeline = {
-  Core: require('./lib/timeline/Core'),
-  DateUtil: require('./lib/timeline/DateUtil'),
-  Range: require('./lib/timeline/Range'),
-  stack: require('./lib/timeline/Stack'),
-  TimeStep: require('./lib/timeline/TimeStep'),
-
-  components: {
-    items: {
-      Item: require('./lib/timeline/component/item/Item'),
-      BackgroundItem: require('./lib/timeline/component/item/BackgroundItem'),
-      BoxItem: require('./lib/timeline/component/item/BoxItem'),
-      PointItem: require('./lib/timeline/component/item/PointItem'),
-      RangeItem: require('./lib/timeline/component/item/RangeItem')
-    },
-
-    BackgroundGroup: require('./lib/timeline/component/BackgroundGroup'),
-    Component: require('./lib/timeline/component/Component'),
-    CurrentTime: require('./lib/timeline/component/CurrentTime'),
-    CustomTime: require('./lib/timeline/component/CustomTime'),
-    DataAxis: require('./lib/timeline/component/DataAxis'),
-    DataScale: require('./lib/timeline/component/DataScale'),
-    GraphGroup: require('./lib/timeline/component/GraphGroup'),
-    Group: require('./lib/timeline/component/Group'),
-    ItemSet: require('./lib/timeline/component/ItemSet'),
-    Legend: require('./lib/timeline/component/Legend'),
-    LineGraph: require('./lib/timeline/component/LineGraph'),
-    TimeAxis: require('./lib/timeline/component/TimeAxis')
-  }
-};
+// exports.Timeline = require('./lib/timeline/Timeline');
+// exports.Graph2d = require('./lib/timeline/Graph2d');
+// exports.timeline = {
+//   Core: require('./lib/timeline/Core'),
+//   DateUtil: require('./lib/timeline/DateUtil'),
+//   Range: require('./lib/timeline/Range'),
+//   stack: require('./lib/timeline/Stack'),
+//   TimeStep: require('./lib/timeline/TimeStep'),
+//
+//   components: {
+//     items: {
+//       Item: require('./lib/timeline/component/item/Item'),
+//       BackgroundItem: require('./lib/timeline/component/item/BackgroundItem'),
+//       BoxItem: require('./lib/timeline/component/item/BoxItem'),
+//       PointItem: require('./lib/timeline/component/item/PointItem'),
+//       RangeItem: require('./lib/timeline/component/item/RangeItem')
+//     },
+//
+//     BackgroundGroup: require('./lib/timeline/component/BackgroundGroup'),
+//     Component: require('./lib/timeline/component/Component'),
+//     CurrentTime: require('./lib/timeline/component/CurrentTime'),
+//     CustomTime: require('./lib/timeline/component/CustomTime'),
+//     DataAxis: require('./lib/timeline/component/DataAxis'),
+//     DataScale: require('./lib/timeline/component/DataScale'),
+//     GraphGroup: require('./lib/timeline/component/GraphGroup'),
+//     Group: require('./lib/timeline/component/Group'),
+//     ItemSet: require('./lib/timeline/component/ItemSet'),
+//     Legend: require('./lib/timeline/component/Legend'),
+//     LineGraph: require('./lib/timeline/component/LineGraph'),
+//     TimeAxis: require('./lib/timeline/component/TimeAxis')
+//   }
+// };
 
 // Network
 exports.Network = require('./lib/network/Network');
